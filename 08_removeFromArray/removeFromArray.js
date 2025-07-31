a491267d7b4772e484e1860d835ee8a471b50f68
Harold Lam <lamwahhinharold@gmail.com>
2025-07-31 14:10:00 +0800

    Complete exercise 08_removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-};
+function removeFromArray(array, ...elementsToRemove) {
+  for (const element of elementsToRemove) {
+    while (array.indexOf(element) !== -1) {
+      const index = array.indexOf(element);
+      array.splice(index, 1);
+    }
+  }
+  return array;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
