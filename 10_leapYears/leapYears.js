fa34be9af888d40307a025edd620384a90125206
Harold Lam <lamwahhinharold@gmail.com>
2025-07-31 15:02:55 +0800

    Complete exercise 10_leapYears

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,6 +1,14 @@
-const leapYears = function() {
-
-};
+function leapYears(year) {
+  if (year % 400 === 0) {
+    return true;
+  } else if (year % 100 === 0 && year % 4 === 0) {
+    return false;
+  } else if (year % 4 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
 // Do not edit below this line
 module.exports = leapYears;
