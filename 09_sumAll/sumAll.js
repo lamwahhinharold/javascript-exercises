f5fc053e1e0b75a0cbd3241f5bf5ccba234e6f52
Harold Lam <lamwahhinharold@gmail.com>
2025-07-31 14:44:28 +0800

    Complete exercise 09_sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,22 @@
-const sumAll = function() {
-
-};
+function sumAll(startInt, endInt) {
+  if (startInt < 0 || endInt < 0) {
+    return "ERROR";
+  } else if (!Number.isSafeInteger(startInt) || !Number.isSafeInteger(endInt)) {
+    return "ERROR";
+  }
+  if (endInt < startInt) {
+    const temp = startInt;
+    startInt = endInt;
+    endInt = temp;
+  }
+  let ctr = startInt;
+  let sum = 0;
+  while (ctr <= endInt) {
+    sum += ctr;
+    ctr++;
+  }
+  return sum;
+}
 
 // Do not edit below this line
 module.exports = sumAll;
