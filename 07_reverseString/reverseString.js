be147cdd29616f70e7a70277ba39422332b7810b
Harold Lam <lamwahhinharold@gmail.com>
2025-07-31 13:11:47 +0800

    Complete exercise 07_reverseString

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,7 @@
-const reverseString = function() {
-
-};
+function reverseString(string) {
+  const reversedString = string.split("").reverse().join("");
+  return reversedString;
+}
 
 // Do not edit below this line
 module.exports = reverseString;
