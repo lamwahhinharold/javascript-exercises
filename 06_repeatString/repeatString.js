b1470e049d5819c07959f1b957159769c23c661d
Harold Lam <lamwahhinharold@gmail.com>
2025-07-31 12:59:59 +0800

    Complete exercise 06_repeatString

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
-};
+function repeatString(string, times) {
+  if (times < 0) {
+    return "ERROR";
+  }
+  let stringToReturn = "";
+  for (let i = 0; i < times; i++) {
+    stringToReturn += string;
+  }
+  return stringToReturn;
+}
 
 // Do not edit below this line
 module.exports = repeatString;
