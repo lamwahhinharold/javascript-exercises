c427267411e5735688498c165657ec83e9ff0c58
Harold Lam <lamwahhinharold@gmail.com>
2025-08-24 17:11:30 +0800

    Complete exercise 13_palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function () {
+function sanitizeString(string) {
+  return string.toLowerCase().replace(/[\s\p{P}]/gu, "");
+}
 
+const palindromes = function (string) {
+  string = sanitizeString(string);
+
+  let leftPtr = 0;
+  let rightPtr = string.length - 1;
+  while (leftPtr < rightPtr) {
+    if (string[leftPtr] !== string[rightPtr]) {
+        return false;
+    }
+    leftPtr++;
+    rightPtr--;
+  }
+  return true;
 };
 
 // Do not edit below this line
