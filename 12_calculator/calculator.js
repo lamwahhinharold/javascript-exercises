bfd605c9c10820f8ade383428dea274fe24709d0
Harold Lam <lamwahhinharold@gmail.com>
2025-08-24 11:55:56 +0800

    Complete exercise 12_calculator

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function (x, y) {
+  return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function (x, y) {
+  return x - y;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  return array.reduce((acc, curr) => acc + curr, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  return array.reduce((acc, curr) => acc * curr, 1);
 };
 
-const power = function() {
-	
+const power = function (x, y) {
+  return x ** y;
 };
 
-const factorial = function() {
-	
+const factorial = function (x) {
+  if (x === 0 || x === 1) {
+    return 1;
+  }
+  let acc = 1;
+  let curr = x;
+  while (curr > 1) {
+    acc *= curr;
+    curr = curr - 1;
+  }
+  return acc;
 };
 
 // Do not edit below this line
@@ -29,5 +38,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
