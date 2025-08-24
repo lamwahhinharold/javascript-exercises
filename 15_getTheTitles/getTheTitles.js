b5cae77ba0f05aaec3460514ae67ca3a72819fca
Harold Lam <lamwahhinharold@gmail.com>
2025-08-24 17:40:08 +0800

    Complete exercise 15_getTheTitles

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,5 +1,8 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (bookObjects) {
+  return bookObjects.reduce((acc, curr) => {
+    acc.push(curr.title);
+    return acc;
+  }, []);
 };
 
 // Do not edit below this line
