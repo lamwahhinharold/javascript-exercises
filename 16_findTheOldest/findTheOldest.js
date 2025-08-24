79cd51aba5329d6c7e74b627a6b2cef7622787fc
Harold Lam <lamwahhinharold@gmail.com>
2025-08-24 18:24:31 +0800

    Complete exercise 16_findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-const findTheOldest = function() {
+const findTheOldest = function (personObjects) {
+  const oldestPerson = personObjects.reduce((acc, curr) => {
+    const currentYear = new Date().getFullYear();
+    let soFarOldestAge, currOldestAge;
+    if (!("yearOfDeath" in acc)) {
+      soFarOldestAge = currentYear - acc.yearOfBirth;
+    } else {
+      soFarOldestAge = acc.yearOfDeath - acc.yearOfBirth;
+    }
+    if (!("yearOfDeath" in curr)) {
+      currOldestAge = currentYear - curr.yearOfBirth;
+    } else {
+      currOldestAge = curr.yearOfDeath - curr.yearOfBirth;
+    }
 
+    if (currOldestAge > soFarOldestAge) {
+      acc = curr;
+    }
+    return acc;
+  });
+  return oldestPerson;
 };
 
 // Do not edit below this line
